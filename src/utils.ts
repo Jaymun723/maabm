9f9868c739671719fc8fbdbd10fba3f58c35bf42
Jaymun723 <jaymun723@yahoo.com>
2020-01-12 13:29:12 +0100

    Added `toPrecision(precision)(number)`

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
 export const eucDiv = (a: number, b: number) => Math.floor(a / b)
 
 export const clamp = (value: number, min: number, max: number) => Math.min(Math.max(min, value), max)
+
+export const toPrecision = (p: number) => (n: number) => Number(n.toPrecision(p))
