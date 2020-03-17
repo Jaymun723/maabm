bf6050b20eb8130559a0ca685703e1cb3508f068
Jaymun723 <jaymun723@yahoo.com>
2020-03-17 22:01:24 +0100

    Added toRange()

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,6 @@ export const clamp = (value: number, min: number, max: number) => Math.min(Math.
 export const toPrecision = (p: number) => (n: number) => Number(n.toPrecision(p))
 
 export const emptyArray = <T>(length: number, baseValue: T) => Array.from({ length }, () => baseValue)
+
+export const toRange = (value: number, startMin: number, startMax: number, endMin: number, endMax: number) =>
+  ((value - startMin) / (startMax - startMin)) * (endMax - endMin) + endMin
